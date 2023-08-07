528f180586caf21a85af74cc9e01213033ef1912
lucaslangsch <lucas.langsch@gmail.com>
2023-08-07 14:45:21 -0300

    feat: add project store-manager

--- src/context/RepoProvider.jsx
+++ src/context/RepoProvider.jsx
@@ -7,7 +7,7 @@ export const RepoContext = createContext();
 
 function RepoProvider({ children }) {
   const [reposGit, setReposGit] = useState(null);
-  const [reposTitle, _setReposTitle] = useState(['trivia', 'recipes-app', 'newpace', 'blogs-api']);
+  const [reposTitle, _setReposTitle] = useState(['trivia', 'recipes-app', 'newpace', 'blogs-api', 'store-manager']);
   const ref = useRef();
 
   useEffect(() => {
--- src/utils/imagesData.js
+++ src/utils/imagesData.js
@@ -8,6 +8,7 @@ const images = {
   newpace,
   'recipes-app': recipes,
   'blogs-api': blog,
+  'store-manager': blog,
 }
 
 export default images;
